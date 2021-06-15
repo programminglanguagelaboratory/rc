50244153ae5b2fc8073de0f9697a17d819981a9c
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-06-15 12:29:21 +0900

    feat: add expr ast types

--- pkg/ast/ast.go
+++ pkg/ast/ast.go
@@ -1,1 +1,30 @@
 package ast
+
+type Expr interface{}
+
+type Id string
+
+type BinOpExpr struct {
+	Left  Expr
+	Right Expr
+	Op    string
+}
+
+type UnOpExpr struct {
+	Left Expr
+	Op   string
+}
+
+type CallExpr struct {
+	Func Expr
+	Args []Expr
+}
+
+type FieldExpr struct {
+	Left  Expr
+	Right Id
+}
+
+type LitExpr struct {
+	Lit string
+}
