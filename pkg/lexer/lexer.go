e0667bac0ebff2a049e4cc81c771dea76630dcad
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-06-15 21:25:40 +0900

    feat: change lexer.lex to yield value

--- pkg/lexer/lexer.go
+++ pkg/lexer/lexer.go
@@ -5,6 +5,6 @@ import "github.com/programminglanguagelaboratory/rc/pkg/token"
 type Lexer struct {
 }
 
-func (lexer *Lexer) lex() *token.Token {
-	return nil
+func (lexer *Lexer) lex() token.Token {
+	return token.Token{}
 }
