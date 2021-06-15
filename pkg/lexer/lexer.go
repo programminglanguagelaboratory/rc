52c9f8d44b44126d5c0691309d8f2474c33b3c19
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-06-15 22:20:11 +0900

    feat: add lexer todo

--- pkg/lexer/lexer.go
+++ pkg/lexer/lexer.go
@@ -5,6 +5,7 @@ import "github.com/programminglanguagelaboratory/rc/pkg/token"
 type Lexer struct {
 }
 
+// TODO: impl error handling
 func (lexer *Lexer) lex() token.Token {
 	return token.Token{}
 }
