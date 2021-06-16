f59ca524cffc20c1ddc56d52fe7efe8c5e886448
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-06-17 01:03:13 +0900

    feat: create parser test framework

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -1,1 +1,19 @@
 package parser
+
+import (
+	"errors"
+
+	"github.com/programminglanguagelaboratory/rc/pkg/ast"
+)
+
+type parser struct {
+}
+
+func newParser() *parser {
+	p := parser{}
+	return &p
+}
+
+func (p *parser) parseExpr() (ast.Expr, error) {
+	return nil, errors.New("not implemented")
+}
--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -1,1 +1,24 @@
 package parser
+
+import (
+	"testing"
+
+	"github.com/programminglanguagelaboratory/rc/pkg/ast"
+)
+
+func TestExpr(t *testing.T) {
+	for _, testcase := range []struct {
+		code     string
+		expected ast.Expr
+	}{} {
+		actual, _ := newParser().parseExpr()
+		if testcase.expected != actual {
+			t.Errorf(
+				"given %v, expected %v, but got actual %v",
+				testcase.code,
+				testcase.expected,
+				actual,
+			)
+		}
+	}
+}
