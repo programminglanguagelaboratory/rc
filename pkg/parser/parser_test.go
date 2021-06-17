984e4cd6067e8ea2a88e0c56296b97e78c63d3c6
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-06-17 21:50:15 +0900

    feat: add expr parser tests

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -5,13 +5,63 @@ import (
 
 	"github.com/programminglanguagelaboratory/rc/pkg/ast"
 	"github.com/programminglanguagelaboratory/rc/pkg/lexer"
+	"github.com/programminglanguagelaboratory/rc/pkg/token"
 )
 
 func TestExpr(t *testing.T) {
 	for _, testcase := range []struct {
 		code     string
 		expected ast.Expr
-	}{} {
+	}{
+		{
+			"10",
+			ast.LitExpr{Token: token.Token{Str: "10", Kind: token.NUMBER}},
+		},
+		{
+			"10 + a",
+			ast.BinOpExpr{
+				Left:  ast.LitExpr{Token: token.Token{Str: "10", Kind: token.NUMBER}},
+				Right: ast.LitExpr{Token: token.Token{Str: "a", Kind: token.ID}},
+				Token: token.Token{Str: "+", Kind: token.PLUS},
+			},
+		},
+		{
+			"10 + 20 * 30",
+			ast.BinOpExpr{
+				Left: ast.LitExpr{Token: token.Token{Str: "10", Kind: token.NUMBER}},
+				Right: ast.BinOpExpr{
+					Left:  ast.LitExpr{Token: token.Token{Str: "20", Kind: token.NUMBER}},
+					Right: ast.LitExpr{Token: token.Token{Str: "30", Kind: token.NUMBER}},
+					Token: token.Token{Str: "*", Kind: token.ASTERISK},
+				},
+				Token: token.Token{Str: "+", Kind: token.PLUS},
+			},
+		},
+		{
+			"(10 + 20) * 30",
+			ast.BinOpExpr{
+				Left: ast.BinOpExpr{
+					Left:  ast.LitExpr{Token: token.Token{Str: "10", Kind: token.NUMBER}},
+					Right: ast.LitExpr{Token: token.Token{Str: "20", Kind: token.NUMBER}},
+					Token: token.Token{Str: "+", Kind: token.PLUS},
+				},
+				Right: ast.LitExpr{Token: token.Token{Str: "30", Kind: token.NUMBER}},
+				Token: token.Token{Str: "*", Kind: token.ASTERISK},
+			},
+		},
+		{
+			"10 - 20 - 30",
+			ast.BinOpExpr{
+				Left: ast.BinOpExpr{
+					Left:  ast.LitExpr{Token: token.Token{Str: "10", Kind: token.NUMBER}},
+					Right: ast.LitExpr{Token: token.Token{Str: "20", Kind: token.NUMBER}},
+					Token: token.Token{Str: "-", Kind: token.MINUS},
+				},
+				Right: ast.LitExpr{Token: token.Token{Str: "30", Kind: token.NUMBER}},
+				Token: token.Token{Str: "-", Kind: token.MINUS},
+			},
+		},
+	} {
 		actual, _ := newParser(lexer.NewLexer(testcase.code)).parseExpr()
 		if testcase.expected != actual {
 			t.Errorf(
