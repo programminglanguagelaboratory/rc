27bb0689683a3a81dfedc6770e73edf75480448e
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-06-21 23:26:17 +0900

    feat: Codegen context type

--- pkg/codegen/codegen.go
+++ pkg/codegen/codegen.go
@@ -3,29 +3,46 @@ package codegen
 import (
 	"errors"
 
+	"github.com/llir/llvm/ir"
+	"github.com/llir/llvm/ir/types"
+
 	"github.com/programminglanguagelaboratory/rc/pkg/ast"
 )
 
-func EmitExpr(e *ast.Expr) interface{} {
+type Codegen struct {
+	mod *ir.Module
+	fun *ir.Func
+	blk *ir.Block
+}
+
+func NewCodegen() *Codegen {
+	c := Codegen{}
+	c.mod = ir.NewModule()
+	c.fun = c.mod.NewFunc("", types.I64)
+	c.blk = c.fun.NewBlock("")
+	return &c
+}
+
+func (c *Codegen) EmitExpr(e *ast.Expr) interface{} {
 	return errors.New("not implemented")
 }
 
-func EmitBinaryExpr(e *ast.BinaryExpr) interface{} {
+func (c *Codegen) EmitBinaryExpr(e *ast.BinaryExpr) interface{} {
 	return errors.New("not implemented")
 }
 
-func EmitUnaryExpr(e *ast.UnaryExpr) interface{} {
+func (c *Codegen) EmitUnaryExpr(e *ast.UnaryExpr) interface{} {
 	return errors.New("not implemented")
 }
 
-func EmitCallExpr(e *ast.CallExpr) interface{} {
+func (c *Codegen) EmitCallExpr(e *ast.CallExpr) interface{} {
 	return errors.New("not implemented")
 }
 
-func EmitFieldExpr(e *ast.FieldExpr) interface{} {
+func (c *Codegen) EmitFieldExpr(e *ast.FieldExpr) interface{} {
 	return errors.New("not implemented")
 }
 
-func EmitLitExpr(e *ast.LitExpr) interface{} {
+func (c *Codegen) EmitLitExpr(e *ast.LitExpr) interface{} {
 	return errors.New("not implemented")
 }
