51ca1ff6fe41601242befd61455d4c4961ce15d1
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-06-21 23:32:25 +0900

    feat: impl genLitExpr

--- pkg/codegen/codegen.go
+++ pkg/codegen/codegen.go
@@ -4,9 +4,12 @@ import (
 	"errors"
 
 	"github.com/llir/llvm/ir"
+	"github.com/llir/llvm/ir/constant"
 	"github.com/llir/llvm/ir/types"
+	"github.com/llir/llvm/ir/value"
 
 	"github.com/programminglanguagelaboratory/rc/pkg/ast"
+	"github.com/programminglanguagelaboratory/rc/pkg/token"
 )
 
 type Codegen struct {
@@ -43,6 +46,14 @@ func (c *Codegen) genFieldExpr(e *ast.FieldExpr) interface{} {
 	return errors.New("not implemented")
 }
 
-func (c *Codegen) genLitExpr(e *ast.LitExpr) interface{} {
-	return errors.New("not implemented")
+func (c *Codegen) genLitExpr(e *ast.LitExpr) value.Value {
+	t := e.Token
+
+	switch t.Kind {
+	case token.NUMBER:
+		val, _ := constant.NewIntFromString(types.I64, t.Text)
+		return val
+	}
+
+	return nil
 }
