64d11cabf93fd56d1e82b3687eb71e11cea06f83
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-06-21 21:10:39 +0900

    feat: add codegen func interfaces

--- pkg/codegen/codegen.go
+++ pkg/codegen/codegen.go
@@ -1,1 +1,31 @@
 package codegen
+
+import (
+	"errors"
+
+	"github.com/programminglanguagelaboratory/rc/pkg/ast"
+)
+
+func EmitExpr(e *ast.Expr) interface{} {
+	return errors.New("not implemented")
+}
+
+func EmitBinaryExpr(e *ast.BinaryExpr) interface{} {
+	return errors.New("not implemented")
+}
+
+func EmitUnaryExpr(e *ast.UnaryExpr) interface{} {
+	return errors.New("not implemented")
+}
+
+func EmitCallExpr(e *ast.CallExpr) interface{} {
+	return errors.New("not implemented")
+}
+
+func EmitFieldExpr(e *ast.FieldExpr) interface{} {
+	return errors.New("not implemented")
+}
+
+func EmitLitExpr(e *ast.LitExpr) interface{} {
+	return errors.New("not implemented")
+}
