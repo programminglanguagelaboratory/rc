6d3c0443f850e4c1cbf6f11bf2eec48b2439b6cd
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-07-14 22:05:16 +0900

    feat: create table.Table type

---
+++ pkg/table/table.go
@@ -0,0 +1,8 @@
+package table
+
+import (
+	"github.com/programminglanguagelaboratory/rc/pkg/symbol"
+	"github.com/programminglanguagelaboratory/rc/pkg/typ"
+)
+
+type Table map[symbol.Symbol]typ.Typ
