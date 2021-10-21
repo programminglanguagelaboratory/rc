03c0dad3be7ea535e94edf08d494a2f28aea0dd9
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-10-21 21:11:55 +0900

    refactor(table): merge symbol pkg into table

--- pkg/symbol/symbol.go
+++
@@ -1,7 +0,0 @@
-package symbol
-
-import "github.com/programminglanguagelaboratory/rc/pkg/token"
-
-type Symbol struct {
-	Tok token.Token
-}
--- pkg/table/table.go
+++ pkg/table/table.go
@@ -1,21 +1,25 @@
 package table
 
 import (
-	"github.com/programminglanguagelaboratory/rc/pkg/symbol"
+	"github.com/programminglanguagelaboratory/rc/pkg/token"
 	"github.com/programminglanguagelaboratory/rc/pkg/typ"
 )
 
-type Table map[symbol.Symbol]typ.Typ
+type Symbol struct {
+	Tok token.Token
+}
+
+type Table map[Symbol]typ.Typ
 
 func NewTable() Table {
 	return make(Table)
 }
 
-func (t *Table) FindType(s symbol.Symbol) (typ.Typ, bool) {
+func (t *Table) FindType(s Symbol) (typ.Typ, bool) {
 	typ, ok := (*t)[s]
 	return typ, ok
 }
 
-func (t *Table) AddType(s symbol.Symbol, typ typ.Typ) {
+func (t *Table) AddType(s Symbol, typ typ.Typ) {
 	(*t)[s] = typ
 }
