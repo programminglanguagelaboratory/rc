06f0de2b8e5879aac6ed0bf6df06511a73573d23
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-11-04 00:05:30 +0900

    feat(desugar): add unaryexpr test

--- pkg/desugar/desugar_test.go
+++ pkg/desugar/desugar_test.go
@@ -1,1 +1,41 @@
 package desugar
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/programminglanguagelaboratory/rc/pkg/ast"
+	"github.com/programminglanguagelaboratory/rc/pkg/token"
+)
+
+func TestBinaryExpr(t *testing.T) {
+}
+
+func TestUnaryExpr(t *testing.T) {
+	expr := ast.UnaryExpr{
+		Left: ast.IdentExpr{
+			Token: token.Token{Text: "a", Kind: token.ID},
+			Value: "a",
+		},
+		Token: token.Token{Text: "+", Kind: token.PLUS},
+	}
+	expected := ast.CallExpr{
+		Func: ast.IdentExpr{
+			Token: token.Token{Text: "+", Kind: token.PLUS},
+			Value: "+",
+		},
+		Arg: ast.IdentExpr{
+			Token: token.Token{Text: "a", Kind: token.ID},
+			Value: "a",
+		},
+	}
+	actual := Desugar(expr)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf(
+			"given %v, expected %v, but got actual %v",
+			expr,
+			expected,
+			actual,
+		)
+	}
+}
