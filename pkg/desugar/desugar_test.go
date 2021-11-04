eba0f6a7a0484f46df7b121d02b0fee403d0faca
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-11-04 12:39:25 +0900

    feat(desugar): add binaryexpr test

--- pkg/desugar/desugar_test.go
+++ pkg/desugar/desugar_test.go
@@ -9,6 +9,42 @@ import (
 )
 
 func TestBinaryExpr(t *testing.T) {
+	expr := ast.BinaryExpr{
+		Left: ast.IdentExpr{
+			Token: token.Token{Text: "a", Kind: token.ID},
+			Value: "a",
+		},
+		Right: ast.IdentExpr{
+			Token: token.Token{Text: "b", Kind: token.ID},
+			Value: "b",
+		},
+		Token: token.Token{Text: "&&", Kind: token.AMPERSANDAMPERSAND},
+	}
+	expected := ast.CallExpr{
+		Func: ast.CallExpr{
+			Func: ast.IdentExpr{
+				Token: token.Token{Text: "&&", Kind: token.AMPERSANDAMPERSAND},
+				Value: "&&",
+			},
+			Arg: ast.IdentExpr{
+				Token: token.Token{Text: "a", Kind: token.ID},
+				Value: "a",
+			},
+		},
+		Arg: ast.IdentExpr{
+			Token: token.Token{Text: "b", Kind: token.ID},
+			Value: "b",
+		},
+	}
+	actual := Desugar(expr)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf(
+			"given %v, expected %v, but got actual %v",
+			expr,
+			expected,
+			actual,
+		)
+	}
 }
 
 func TestUnaryExpr(t *testing.T) {
