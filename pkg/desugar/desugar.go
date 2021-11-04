e01eb82f02fa4dd835347b9c255e4504c19c85f8
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-11-04 18:11:31 +0900

    feat(desugar): impl

--- pkg/desugar/desugar.go
+++ pkg/desugar/desugar.go
@@ -2,6 +2,36 @@ package desugar
 
 import "github.com/programminglanguagelaboratory/rc/pkg/ast"
 
-func Desugar(expr ast.Expr) ast.Expr {
-	return nil
+func Desugar(e ast.Expr) ast.Expr {
+	switch v := interface{}(e).(type) {
+	case ast.BinaryExpr:
+		return desugarBinaryExpr(v)
+	case ast.UnaryExpr:
+		return desugarUnaryExpr(v)
+	default:
+		return v
+	}
+}
+
+func desugarBinaryExpr(e ast.BinaryExpr) ast.CallExpr {
+	return ast.CallExpr{
+		Func: ast.CallExpr{
+			Func: ast.IdentExpr{
+				Token: e.Token,
+				Value: e.Token.Text,
+			},
+			Arg: e.Left,
+		},
+		Arg: e.Right,
+	}
+}
+
+func desugarUnaryExpr(e ast.UnaryExpr) ast.CallExpr {
+	return ast.CallExpr{
+		Func: ast.IdentExpr{
+			Token: e.Token,
+			Value: e.Token.Text,
+		},
+		Arg: e.Left,
+	}
 }
