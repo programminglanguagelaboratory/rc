678b40831d6501bbdacdedb5d151a8a4a7b58dfd
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-12-05 19:30:34 +0900

    feat(typcheck): impl Substitutable for inferTyp

--- pkg/typcheck/inferTyp.go
+++ pkg/typcheck/inferTyp.go
@@ -7,29 +7,44 @@ import (
 )
 
 type inferTyp interface {
-	inferType()
 	Substitutable
 	fmt.Stringer
+	inferType()
 }
 
 type varType struct {
 	tv string
 }
 
-func (t *varType) inferType()     {}
+func (t *varType) Apply(s Subst) Substitutable {
+	c, ok := map[string]typ.Typ(s)[t.tv]
+	if !ok {
+		return Substitutable(t)
+	}
+	return Substitutable(&constTyp{t: c})
+}
 func (t *varType) String() string { return t.tv }
+func (t *varType) inferType()     {}
 
-type constType struct {
+type constTyp struct {
 	t typ.Typ
 }
 
-func (t *constType) inferType()     {}
-func (t *constType) String() string { return t.t.String() }
+func (t *constTyp) Apply(s Subst) Substitutable {
+	return Substitutable(t)
+}
+func (t *constTyp) String() string { return t.t.String() }
+func (t *constTyp) inferType()     {}
 
 type funcTyp struct {
 	from inferTyp
 	to   inferTyp
 }
 
+func (t *funcTyp) Apply(s Subst) Substitutable {
+	t.from = t.from.Apply(s).(inferTyp)
+	t.to = t.to.Apply(s).(inferTyp)
+	return Substitutable(t)
+}
 func (t *funcTyp) inferType()     {}
 func (t *funcTyp) String() string { return t.from.String() + " -> " + t.to.String() }
