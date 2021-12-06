ec1960e2deb6fe5c8ac978dfb5896a4a1c9e9595
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-12-06 23:33:33 +0900

    feat(typcheck): impl Substitutable for scheme

--- pkg/typcheck/typcheck.go
+++ pkg/typcheck/typcheck.go
@@ -17,6 +17,44 @@ func (c *context) FreeTypeVars() Substitutable {
 	panic("not impl")
 }
 
+type scheme struct {
+	tvs []string
+	t   inferTyp
+}
+
+func (s scheme) Apply(subst Subst) Substitutable {
+	var freeSubst Subst
+	for tv, t := range subst {
+		freeSubst[tv] = t
+	}
+	for _, tv := range s.tvs {
+		delete(freeSubst, tv)
+	}
+
+	return Substitutable(&scheme{
+		tvs: s.tvs,
+		t:   s.t.Apply(freeSubst).(inferTyp),
+	})
+}
+
+func (s scheme) FreeTypeVars() []string {
+	tvs := make(map[string]struct{})
+	for _, tv := range s.t.FreeTypeVars() {
+		tvs[tv] = struct{}{}
+	}
+	for _, tv := range s.tvs {
+		delete(tvs, tv)
+	}
+
+	ret := make([]string, len(tvs))
+	i := 0
+	for tv := range tvs {
+		ret[i] = tv
+		i++
+	}
+	return ret
+}
+
 func Infer(e ast.Expr) (typ.Typ, error) {
 	return inferExpr(e)
 }
