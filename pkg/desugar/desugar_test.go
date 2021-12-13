12f00d16517fdc822a022d5031b2748179bbc0c1
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-12-13 23:21:15 +0900

    feat(desugar): add nested desugar test

--- pkg/desugar/desugar_test.go
+++ pkg/desugar/desugar_test.go
@@ -10,32 +10,70 @@ import (
 )
 
 func TestBinaryExpr(t *testing.T) {
-	code := "a && b"
-	expr, _ := parser.Parse(code)
-	expected := ast.CallExpr{
-		Func: ast.CallExpr{
-			Func: ast.IdentExpr{
-				Token: token.Token{Text: "&&", Kind: token.AMPERSANDAMPERSAND},
-				Value: "&&",
-			},
-			Arg: ast.IdentExpr{
-				Token: token.Token{Text: "a", Kind: token.ID},
-				Value: "a",
+	for _, tt := range []struct {
+		code     string
+		expected ast.Expr
+	}{
+		{
+			code: "a && b",
+			expected: ast.CallExpr{
+				Func: ast.CallExpr{
+					Func: ast.IdentExpr{
+						Token: token.Token{Text: "&&", Kind: token.AMPERSANDAMPERSAND},
+						Value: "&&",
+					},
+					Arg: ast.IdentExpr{
+						Token: token.Token{Text: "a", Kind: token.ID},
+						Value: "a",
+					},
+				},
+				Arg: ast.IdentExpr{
+					Token: token.Token{Text: "b", Kind: token.ID},
+					Value: "b",
+				},
 			},
 		},
-		Arg: ast.IdentExpr{
-			Token: token.Token{Text: "b", Kind: token.ID},
-			Value: "b",
+		{
+			code: "10 + 20 * 30",
+			expected: ast.CallExpr{
+				Func: ast.CallExpr{
+					Func: ast.IdentExpr{
+						Token: token.Token{Text: "+", Kind: token.PLUS},
+						Value: "+",
+					},
+					Arg: ast.NumberExpr{
+						Token: token.Token{Text: "10", Kind: token.NUMBER},
+						Value: 10,
+					},
+				},
+				Arg: ast.CallExpr{
+					Func: ast.CallExpr{
+						Func: ast.IdentExpr{
+							Token: token.Token{Text: "*", Kind: token.ASTERISK},
+							Value: "*",
+						},
+						Arg: ast.NumberExpr{
+							Token: token.Token{Text: "20", Kind: token.NUMBER},
+							Value: 20,
+						},
+					},
+					Arg: ast.NumberExpr{
+						Token: token.Token{Text: "30", Kind: token.NUMBER},
+						Value: 30,
+					},
+				},
+			},
 		},
-	}
-	actual := Desugar(expr)
-	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf(
-			"given %v, expected %v, but got actual %v",
-			expr,
-			expected,
-			actual,
-		)
+	} {
+		expr, err := parser.Parse(tt.code)
+		if err != nil {
+			t.Errorf("given %v, expected %v, but got an error: %v", tt.code, tt.expected, err)
+			continue
+		}
+		actual := Desugar(expr)
+		if !reflect.DeepEqual(tt.expected, actual) {
+			t.Errorf("given %v, expected %v, but got actual %v", tt.code, tt.expected, actual)
+		}
 	}
 }
 
@@ -54,11 +92,6 @@ func TestUnaryExpr(t *testing.T) {
 	}
 	actual := Desugar(expr)
 	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf(
-			"given %v, expected %v, but got actual %v",
-			expr,
-			expected,
-			actual,
-		)
+		t.Errorf("given %v, expected %v, but got actual %v", expr, expected, actual)
 	}
 }
